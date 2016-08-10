f65f1161578db408b0a0c82cc9430431f6410c91
JessieLabadan07 <jessielabadan07@gmail.com>
2016-08-10 08:39:29 +0800

    Pushed additional files.

---
+++ src/ipromise.js
@@ -0,0 +1,26 @@
+var iPromise = ( function ( res, rej ) {
+
+	return new Promise ( function (resolve, reject) { 		
+		if ( resolve ) resolve(res);
+		if ( reject ) reject(rej);
+
+	});
+
+});
+
+var ip = iPromise(res, rej);
+
+ip.then(res, rej);
+
+function res() {
+	console.log('inside resolve');
+};
+
+function rej() {
+	console.log('inside reject');
+};
+
+/*iPromise(
+	res, 
+	rej
+); */
