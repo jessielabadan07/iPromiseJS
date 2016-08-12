44b239860dc9fb6b3c5e344b023ab9244702d039
JessieLabadan07 <jessielabadan07@gmail.com>
2016-08-12 08:01:08 +0800

    Formatted scripts

--- src/ipromise.js
+++ src/ipromise.js
@@ -1,14 +1,14 @@
 /**
-* iPromise library
-*
-**/
+ * iPromise library
+ *
+ **/
 
 var iPromise = (function(res, rej) {
 
-    return new Promise(function(resolve, reject) {
-        if (resolve) resolve(res);
-        if (reject) reject(rej);
+	return new Promise(function(resolve, reject) {
+		if (resolve) resolve(res);
+		if (reject) reject(rej);
 
-    });
+	});
 
 });
